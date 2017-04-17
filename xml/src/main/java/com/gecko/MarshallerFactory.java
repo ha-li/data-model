9550e5813830bf1e48e4ba99465eb414459348b9
ha-li <ha-li@gmail.com>
2017-04-16 20:55:44 -0700

    add string writer instead of system out

--- xml/src/main/java/com/gecko/MarshallerFactory.java
+++ xml/src/main/java/com/gecko/MarshallerFactory.java
@@ -1,24 +1,29 @@
 package com.gecko;
 
 import com.gecko.domain.internal.Animal;
+import com.gecko.domain.internal.CreditCard;
 import com.gecko.domain.internal.Inventory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
 
 /**
  * Created by hlieu on 04/16/17.
  */
 public class MarshallerFactory {
 
-   public static void marshal (Object object) throws Exception {
+   public static String marshal (Object object) throws Exception {
       JAXBContext context = JAXBContext.newInstance (object.getClass());
       Marshaller marshaller = context.createMarshaller ();
       marshaller.setProperty (Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-      marshaller.marshal (object, System.out);
+
+      StringWriter sw = new StringWriter ();
+      marshaller.marshal (object, sw);
+      return sw.toString ();
    }
 
-   public static void main (String[] args) throws Exception {
+   public static Inventory getInventory () {
       Inventory inventory = new Inventory ();
       Animal animal = new Animal ();
       animal.setAnimalClass ("mammal");
@@ -29,8 +34,23 @@ public class MarshallerFactory {
       animal.setWeight (45.0);
       animal.setFoodRecipe (null);
       inventory.getAnimal().add(animal);
+      return inventory;
+   }
 
-      MarshallerFactory.marshal (animal);
+   public static CreditCard getCreditCard () {
+      CreditCard cc = new CreditCard ();
+      cc.setType ("mastercard");
+      cc.setControlNumber (342);
+      cc.setExpiryDate ("3/20");
+      cc.setNumber ("234234234234");
+      return cc;
+   }
+
+   public static void main (String[] args) throws Exception {
+
+      Inventory inventory = getInventory ();
+      System.out.println (MarshallerFactory.marshal (inventory.getAnimal ().get(0)));
+      //MarshallerFactory.marshal (getCreditCard ());
    }
 }
 
