f546bb7353127630e444f2c2c2d7b19dd6fb4f1a
ha-li <ha-li@gmail.com>
2017-04-16 22:13:36 -0700

    fix packaging

--- xml/src/main/java/com/gecko/domain/Inventory.java
+++ xml/src/main/java/com/gecko/domain/Inventory.java
@@ -3,7 +3,7 @@ package com.gecko.domain;
 /**
  * Created by hlieu on 04/16/17.
  */
-public class Inventory extends com.gecko.domain.internal.Inventory {
+public class Inventory extends com.gecko.schema.anything.v1.Inventory {
    public Inventory () {
       super();
    }
