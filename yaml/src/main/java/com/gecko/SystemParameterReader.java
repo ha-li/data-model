571625ceaf6c50e3f09e620ce1ba184e64732f33
ha-li <ha-li@gmail.com>
2017-04-26 00:37:39 -0700

    clean up

--- yaml/src/main/java/com/gecko/SystemParameterReader.java
+++ yaml/src/main/java/com/gecko/SystemParameterReader.java
@@ -1,7 +1,6 @@
 package com.gecko;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
-import com.gecko.domain.EnvironmentValue;
 import com.gecko.domain.SystemParameter;
 import com.gecko.domain.SystemParameters;
 
@@ -20,14 +19,7 @@ public class SystemParameterReader {
       YamlReader yamlReader = new YamlReader (new FileReader (fileName));
       yamlReader.getConfig().setClassTag("SystemParameters", SystemParameters.class);
       yamlReader.getConfig().setClassTag("SystemParameter", SystemParameter.class);
-      //yamlReader.getConfig().setClassTag("EnvironmentValue", EnvironmentValue.class);
-      //yamlReader.getConfig ().setPropertyElementType(SystemParameter.class, "envs", EnvironmentValue.class);
       SystemParameters all = yamlReader.read(SystemParameters.class, List.class);
-      // List all = yamlReader.read(List.class, SystemParameter.class);
-
-      //for(Object sys : all) {
-         //SystemParameter s = (SystemParameter) sys;
-         System.out.println (all);
-      //}
+      System.out.println (all);
    }
 }
